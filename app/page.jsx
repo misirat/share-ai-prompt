1b41165bc13739543b4e73044de2b5899b0e87ff
Md Mohaiminul Islam <mi@netzlab.de>
2024-03-04 17:08:42 +0100

    set up proj, folders and assets

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+const Home = () => {
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Discover & Share
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">
+                    AI-Powered Prompts
+                </span>
+            </h1>
+            <p className="desc text-center">
+                PlotForge is an open source AI prompting tool or modern world to discover, share and create creative prompts.
+            </p>
+            {/* :TODO: FEED COMPONENT */}
+        </section>
+    )
+}
+
+export default Home
