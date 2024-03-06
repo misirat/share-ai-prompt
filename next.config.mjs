b98bbbfa88a42ae5c6621e98db6dc40492f0a139
Md Mohaiminul Islam <mi@netzlab.de>
2024-03-06 15:46:56 +0100

    fix deprecated image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["lh3.googleusercontent.com"],
+        // domains: ["lh3.googleusercontent.com"],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                pathname: '**',
+            },
+        ],
     },
     webpack(config) {
         config.experiments = {
