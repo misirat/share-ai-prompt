c866be82a25a316a0e4de7231b1dbccd7601a895
Md Mohaiminul Islam <mi@netzlab.de>
2024-03-07 22:15:22 +0100

    add feed page, fix router error

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+'use client'
+import { useState, useEffect } from "react"
+import PromptCard from "./PromptCard"
+
+const PromptCardList = ({ data, handleTagClick }) => {
+    return (
+        <div className="mt-16 prompt_layout">
+            {data.map((post) => (
+                <PromptCard
+                    key={post._id}
+                    post={post}
+                    handleTagClick={handleTagClick}
+                />
+            )
+            )}
+        </div>
+    )
+}
 
 const Feed = () => {
+    const [searchText, setSearchText] = useState("")
+    const [posts, setPosts] = useState([])
+    const handleSearchChange = (event) => {
+    }
+
+    useEffect(() => {
+
+        const fetchPosts = async () => {
+            const response = await fetch('/api/prompt')
+            const data = await response.json()
+            setPosts(data)
+        }
+
+        fetchPosts()
+    }, [])
+
     return (
-        <div>Feed</div>
+        <section className="feed">
+            <form action="" className="relative w-full flex-center">
+                <input
+                    className="search_input peer"
+                    type="text"
+                    placeholder="search for a tag/prompt/username"
+                    value={searchText}
+                    onChange={handleSearchChange}
+                    required />
+            </form>
+
+            <PromptCardList
+                data={posts}
+                handleTagClick={() => { }}
+            />
+        </section>
     )
 }
 
