72b4ceee524dd39f3e1fdcebb4d3fe14e000a792
Md Mohaiminul Islam <mi@netzlab.de>
2024-03-09 21:33:16 +0100

    mongodb url change

--- utils/database.js
+++ utils/database.js
@@ -11,7 +11,7 @@ export const connectToDb = async () => {
     }
 
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
+        await mongoose.connect("mongodb+srv://misirat:LkZNmT-je3sP-jK@cluster0.qy4nl1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
             dbName: 'db_plot_forge',
             useNewUrlParser: true,
             useUnifiedTopology: true
