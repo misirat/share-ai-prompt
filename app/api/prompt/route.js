72cb0a5907540b3a5d7aff4505c781fd8f3f4754
Md Mohaiminul Islam <mi@netzlab.de>
2024-03-11 20:32:19 +0100

    fix dynamic rendering

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,6 +1,7 @@
 import Prompt from "@models/prompt";
 import { connectToDb } from "@utils/database";
 
+export const dynamic = 'force-dynamic'
 export const GET = async (req) => {
     try {
         await connectToDb();
